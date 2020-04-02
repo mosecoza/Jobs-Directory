b1ec833f86ff6602919e936307240ce4bab43e68
Bonginkosi Lukhele <buildhealth@bonginkosimac.local>
2020-04-02 02:19:49 +0200

    - card header

--- src/components/ListCard/ListCard.jsx
+++ src/components/ListCard/ListCard.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
     Card,
     CardHeader,
@@ -9,7 +9,7 @@ import {
     Button,
     Col
 } from "shards-react";
-import {ReactComponent as Placeholder} from "../../assets/placeholder.svg";
+import { ReactComponent as Placeholder } from "../../assets/placeholder.svg";
 
 export default function CardBodyTitleSubtitleExample({ id, type, url, created_at, company, company_url, location, title, description, how_to_apply, company_logo }) {
     var parser = new DOMParser();
@@ -21,36 +21,38 @@ export default function CardBodyTitleSubtitleExample({ id, type, url, created_at
 
     useEffect(() => {
         if (spanRef.current) {
-          spanRef.current.innerHTML = someHTML;
+            spanRef.current.innerHTML = someHTML;
         }
-      }, [spanRef.current]);
+    }, [spanRef.current]);
 
     // Parse the text
     var doc = parser.parseFromString(html, "text/html");
     // console.log(doc)
     return (
-            <Card className='card' style={{ maxWidth: 300,margin: 4 }}>
-                <CardHeader className='text-truncate d-flex justify-content-between'>
-                {company_logo?
-                <img className='bd-placeholder-img card-img-top ml-1 mr-1' style={{ backgroundColor: 'grey',imageRendering: 'crisp-edges', width: 30, height: 30, objectFit: "cover" }} src={company_logo} alt='company logo' />:
-                <Placeholder style={{ backgroundColor: 'grey',imageRendering: 'crisp-edges', width: 30, height: 30 }} className='bd-placeholder-img card-img-top ml-1 mr-1'/>
+        <Card className='card' style={{ maxWidth: 300, margin: 4 }}>
+            <div className='text-truncate d-flex justify-content-between d-flex align-items-center card-header bg-white'>
+                {company_logo ?
+                    <img className='bd-placeholder-img card-img-top ml-1 mr-1' style={{ backgroundColor: 'grey', imageRendering: 'crisp-edges', width: 30, height: 30, objectFit: "cover" }} src={company_logo} alt='company logo' /> :
+                    <Placeholder style={{ backgroundColor: 'grey', imageRendering: 'crisp-edges', width: 30, height: 30 }} className='bd-placeholder-img card-img-top ml-1 mr-1' />
                 }
                 <div className='d-flex flex-column'>
-                    <div className='font-weight-bold'>
-                    {title}
-                    </div>
+                    <h6 className='font-weight-bold'>
+                        {title}
+                    </h6>
+                    <p className="font-weight-light">
                     {created_at}
+                    </p>
                 </div>
-                    </CardHeader>
-                
-                <CardBody className='card-body'>
-                    <CardTitle>{company}</CardTitle>
-                    <p>{type}</p>
-                    <span className='h-5 text-truncate' ref={spanRef} />
-                    <Button>Read more &rarr;</Button>
-                </CardBody>
-                <CardFooter>{location}</CardFooter>
-            </Card>
+            </div>
+
+            <div className='card-body'>
+                <CardTitle>{company}</CardTitle>
+                <p>{type}</p>
+                <span className='h-5 text-wrap text-truncate' ref={spanRef} />
+                <Button>Read more &rarr;</Button>
+            </div>
+            <CardFooter>{location}</CardFooter>
+        </Card>
 
     );
 }
